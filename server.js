6cbb459da70c8b8b4cbeb7367846e06262a96b38
Ky-Ann Long <kyannlong@gmail.com>
2019-05-08 18:42:42 -0400

    server.js changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -21,21 +22,21 @@ app.use(bodyParser.json());
 const db = 'mongodb://localhost/mern_authenticate_me';
 
 // Connect to MongoDB
-// mongoose
-//   .connect(
-//     db,
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-  mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://password1@ds153566.mlab.com:53566/heroku_8pq8xsmq",
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true
-    }
-  );
+  // mongoose.connect(
+  //   process.env.MONGODB_URI || "mongodb://password1@ds153566.mlab.com:53566/heroku_8pq8xsmq",
+  //   {
+  //     useCreateIndex: true,
+  //     useNewUrlParser: true
+  //   }
+  // );
 
 // Passport middleware
 app.use(passport.initialize());
@@ -47,7 +48,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(Express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
@@ -60,3 +61,45 @@ app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const passport = require("passport");
+
+// const users = require("./routes/api/users");
+
+// const app = express();
+
+// // Bodyparser middleware
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(bodyParser.json());
+
+// // DB Config
+// const db = require("./config/keys").mongoURI;
+
+// // Connect to MongoDB
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+
+// // Passport middleware
+// app.use(passport.initialize());
+
+// // Passport config
+// require("./config/passport")(passport);
+
+// // Routes
+// app.use("/api/users", users);
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server up and running on port ${port} !`));
